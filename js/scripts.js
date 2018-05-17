83aab0e48c3b008635604d912039b38e1648e854
Guest User <Guest@Epicodus-10E.local>
2018-05-16 20:35:45 -0700

    add new directories for my peekaboo project

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("p").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+  $("p1").click(function() {
+    $(".lobster-showing").toggle();
+    $(".lobster-hidden").toggle();
+  });
+});
